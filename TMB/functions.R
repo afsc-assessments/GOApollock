8f73f42fc3a63da4d600f1ada72c06b1b33686ea
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2023-08-15 15:54:13 -0700

    Add code to TMB demo to stabilize optimization

--- TMB/functions.R
+++ TMB/functions.R
@@ -41,3 +41,28 @@ plot_osa_comps <- function(obs, exp, ages, years, Neff, model,
   }
   return(invisible(res))
 }
+
+
+#' Calculate bounds for parameters
+#' @param obj Object
+#' @return list of lower (lwr) and upper (upr) vectors matching
+#'   the object
+get_bounds <- function(obj){
+  lwr <- obj$par-Inf
+  upr <- obj$par+Inf
+  upr['log_slp2_fsh_mean'] <- 5
+  lwr['log_slp2_fsh_mean'] <- -5
+  upr['inf2_fsh_mean'] <- 15
+  lwr['inf2_fsh_mean'] <- 7
+  upr['log_slp2_srv1'] <- 5
+  lwr['log_slp2_srv1'] <- -5
+  upr['inf2_srv1'] <- 20#10
+  lwr['inf2_srv1'] <- 3
+  upr['log_slp1_srv2'] <- 5
+  lwr['log_slp1_srv2'] <- -5
+  upr['inf1_srv2'] <- 50
+  lwr['inf1_srv2'] <- 1
+  upr['log_q2_mean'] <- 10
+  lwr['log_q2_mean'] <- -10
+  return(list(lwr=lwr, upr=upr))
+}
