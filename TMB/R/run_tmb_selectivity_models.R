855e7e39ba80b820fc0b190f2aa3ec6d542c630d
grantdadams <grantadams60091@gmail.com>
2023-08-17 12:31:20 -0700

    fixed tmb helper

--- TMB/R/run_tmb_selectivity_models.R
+++ TMB/R/run_tmb_selectivity_models.R
@@ -53,8 +53,8 @@ obj_mod1 <- MakeADFun(data=dat, parameters=pars, map=map_mod1, random=random, si
 # - Optimize
 lwr <- get_bounds(obj_mod1)$lwr
 upr <- get_bounds(obj_mod1)$upr
-opt_mod1 <- with(obj_mod1, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj)
+#opt_mod1 <- with(obj_mod1, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod1 <- TMBhelper::fit_tmb(obj_mod1, upper = upr, lower = lwr, quiet = TRUE)
 sdrep_mod1 <- sdreport(obj_mod1)
 quantities_mod1 <- obj_mod1$report(obj_mod1$env$last.par.best)
 
@@ -104,8 +104,8 @@ obj_mod2 <- MakeADFun(data=dat, parameters=pars, map=map_mod2, random=random, si
 # - Optimize
 lwr <- get_bounds(obj_mod2)$lwr
 upr <- get_bounds(obj_mod2)$upr
-opt_mod2 <- with(obj_mod2, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj)
+#opt_mod2 <- with(obj_mod2, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod2 <- TMBhelper::fit_tmb(obj_mod2, lower=lwr, upper=upr)
 sdrep_mod2 <- sdreport(obj_mod2)
 quantities_mod2 <- obj_mod2$report(obj_mod2$env$last.par.best)
 
@@ -157,8 +157,8 @@ obj_mod3 <- MakeADFun(data=dat, parameters=pars_mod3, map=map_mod3, random=rando
 # - Optimize
 lwr <- get_bounds(obj_mod3)$lwr
 upr <- get_bounds(obj_mod3)$upr
-opt_mod3 <- with(obj_mod3, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj)
+#opt_mod3 <- with(obj_mod3, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod3 <- TMBhelper::fit_tmb(obj_mod3, lower=lwr, upper=upr)
 sdrep_mod3 <- sdreport(obj_mod3)
 quantities_mod3 <- obj_mod3$report(obj_mod3$env$last.par.best)
 
@@ -195,24 +195,24 @@ dat$seltype <- 4
 random <- c("selpars_re")
 
 # -- Phase
-phased_pars_mod4 <- TMBphase(
-  data = dat,
-  parameters = pars_mod4,
-  map = map_mod4,
-  random = random,
-  phases = phases,
-  model_name = "goa_pk_tmb",
-  silent = TRUE,
-  use_gradient = TRUE)
+# phased_pars_mod4 <- TMBphase(
+#   data = dat,
+#   parameters = pars_mod4,
+#   map = map_mod4,
+#   random = random,
+#   phases = phases,
+#   model_name = "goa_pk_tmb",
+#   silent = TRUE,
+#   use_gradient = TRUE)
 
 # -- Build model obj
-obj_mod4 <- MakeADFun(data=dat, parameters=phased_pars_mod4, map=map_mod4, random=random, silent=TRUE)
+obj_mod4 <- MakeADFun(data=dat, parameters=pars_mod4, map=map_mod4, random=random, silent=TRUE)
 
 # - Optimize
 lwr <- get_bounds(obj_mod4)$lwr
 upr <- get_bounds(obj_mod4)$upr
-opt_mod4 <- with(obj_mod4, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj)
+#opt_mod4 <- with(obj_mod4, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod4 <- TMBhelper::fit_tmb(obj_mod4, lower=lwr, upper=upr)
 sdrep_mod4 <- sdreport(obj_mod4)
 quantities_mod4 <- obj_mod4$report(obj_mod4$env$last.par.best)
 
@@ -258,8 +258,8 @@ obj_mod5 <- MakeADFun(data=dat, parameters=pars, map=map_mod5, random=random, si
 # - Optimize
 lwr <- get_bounds(obj_mod5)$lwr
 upr <- get_bounds(obj_mod5)$upr
-opt_mod5 <- with(obj_mod5, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj)
+#opt_mod5 <- with(obj_mod5, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod5 <- TMBhelper::fit_tmb(obj_mod5, lower=lwr, upper=upr)
 sdrep_mod5 <- sdreport(obj_mod5)
 quantities_mod5 <- obj_mod5$report(obj_mod5$env$last.par.best)
 
@@ -307,8 +307,8 @@ obj_mod6 <- MakeADFun(data=dat, parameters=pars_mod6, map=map_mod6, random=rando
 # - Optimize
 lwr <- get_bounds(obj_mod6)$lwr
 upr <- get_bounds(obj_mod6)$upr
-opt_mod6 <- with(obj_mod6, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj)
+#opt_mod6 <- with(obj_mod6, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod6 <- TMBhelper::fit_tmb(obj_mod6, lower=lwr, upper=upr)
 sdrep_mod6 <- sdreport(obj_mod6)
 quantities_mod6 <- obj_mod6$report(obj_mod6$env$last.par.best)
 
@@ -357,8 +357,8 @@ obj_mod7 <- MakeADFun(data=dat, parameters=pars_mod7, map=map_mod7, random=rando
 # - Optimize
 lwr <- get_bounds(obj_mod7)$lwr
 upr <- get_bounds(obj_mod7)$upr
-opt_mod7 <- with(obj_mod7, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj_mod7)
+#opt_mod7 <- with(obj_mod7, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod7 <- TMBhelper::fit_tmb(obj_mod7, lower=lwr, upper=upr)
 sdrep_mod7 <- sdreport(obj_mod7)
 quantities_mod7 <- obj_mod7$report(obj_mod7$env$last.par.best)
 
@@ -409,8 +409,8 @@ obj_mod8 <- MakeADFun(data=dat, parameters=pars_mod8, map=map_mod8, random=rando
 # - Optimize
 lwr <- get_bounds(obj_mod8)$lwr
 upr <- get_bounds(obj_mod8)$upr
-opt_mod8 <- with(obj_mod8, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj_mod8)
+#opt_mod8 <- with(obj_mod8, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod8 <- TMBhelper::fit_tmb(obj_mod8, lower=lwr, upper=upr)
 sdrep_mod8 <- sdreport(obj_mod8)
 quantities_mod8 <- obj_mod8$report(obj_mod8$env$last.par.best)
 
@@ -461,8 +461,8 @@ obj_mod9 <- MakeADFun(data=dat, parameters=pars_mod9, map=map_mod9, random=rando
 # - Optimize
 lwr <- get_bounds(obj_mod9)$lwr
 upr <- get_bounds(obj_mod9)$upr
-opt_mod9 <- with(obj_mod9, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
-# opt <- TMBhelper::fit_tmb(obj_mod9)
+#opt_mod9 <- with(obj_mod9, nlminb(par,fn, gr, control = control, lower=lwr, upper=upr))
+helper_opt_mod9 <- TMBhelper::fit_tmb(obj_mod9, lower=lwr, upper=upr)
 sdrep_mod9 <- sdreport(obj_mod9)
 quantities_mod9 <- obj_mod9$report(obj_mod9$env$last.par.best)
 
@@ -471,8 +471,10 @@ stdtmb_mod9 <- with(sdrep_mod9, data.frame(par=names(value), est=value, se=sqrt(
   group_by(par) %>% mutate(year=1969+1:n()) %>% ungroup
 params_mod9 <- obj_mod9$env$parList()
 
+save.image(file='TMB/Selectivity_runs_tmbhelper.RData')
 
-## PLOTS ----
+
+## Combine objects ----
 model_names <- c(
   'TMB-Mod1: RE',
   'TMB-Mod2: Log-AR1-Age',
@@ -485,9 +487,13 @@ model_names <- c(
   'TMB-Mod9: 3D-AR1mar'
 )
 
+# - Sdrep objects
 mod_list <- list(sdrep_mod1, sdrep_mod2, sdrep_mod3, sdrep_mod4, sdrep_mod5, sdrep_mod6, sdrep_mod7, sdrep_mod8, sdrep_mod9)
 
-# - Combine
+# - Opt objects
+opt_list <- list(opt_mod1, opt_mod2, opt_mod3, opt_mod4, opt_mod5, opt_mod6, opt_mod7, opt_mod8, opt_mod9)
+
+# - ssb
 ssb <- rbind(cbind(model=model_names[1],filter(stdtmb_mod1, par=='Espawnbio')),
              cbind(model=model_names[2],filter(stdtmb_mod2, par=='Espawnbio')),
              cbind(model=model_names[3],filter(stdtmb_mod3, par=='Espawnbio')),
@@ -499,19 +505,37 @@ ssb <- rbind(cbind(model=model_names[1],filter(stdtmb_mod1, par=='Espawnbio')),
              cbind(model=model_names[9],filter(stdtmb_mod9, par=='Espawnbio')),
              cbind(model='ADMB',filter(stdadmb, name=='Espawnbio') %>% select(par=name, est,se, year)))
 
-# * Plot SSB ----
-ggplot(ssb, aes(year, est, color=model, fill=model, ymin=est-1.96*se, ymax=est+1.96*se)) +
-  geom_ribbon(alpha=.5) + geom_line(lwd=2)
 
-write.csv(ssb, file = "TMB/Output/Initial_runs.csv")
+## TABLES ----
+write.csv(ssb, file = "TMB/Output/Selectivity_runs_ssb_full_estimation.csv")
 
 # - Show recent ssb
-ssb %>%
+save_ssb <- ssb %>%
   filter(year>2015) %>%
   arrange(year) %>%
   pivot_wider(names_from = model, values_from = c(est, se)) %>%
   select(year, paste0(c("est_","se_"),rep(unique(ssb$model), each = 2))) %>%
   as.data.frame()
+write.csv(save_ssb, file = "TMB/Output/Selectivity_runs_terminal_ssb_full_estimation.csv")
+
+# - AIC
+aic_table <- data.frame(
+  model = model_names,
+  nll = sapply(opt_list, function(x) x$objective),
+  k = sapply(opt_list, function(x) length(x$par)),
+  AIC = sapply(opt_list, function(x) TMBAIC(x))
+) %>%
+  mutate(dAIC = AIC - min(AIC))
+
+write.csv(aic_table, file = "TMB/Output/Selectivity_runs_AIC_full_estimation.csv")
+
+
+save.image(file='TMB/Selectivity_runs_phased.RData')
+
+## PLOTS ----
+# * Plot SSB ----
+ggplot(ssb, aes(year, est, color=model, fill=model, ymin=est-1.96*se, ymax=est+1.96*se)) +
+  geom_ribbon(alpha=.5) + geom_line(lwd=2)
 
 # * Plot selectivity ----
 plot_selectivity(sdrep = mod_list[-6],
@@ -523,15 +547,26 @@ quantities_list <- list(quantities_mod1, quantities_mod2, quantities_mod3, quant
 r_osa_list <- list()
 g_osa_list <- list()
 
+# - ADMB
+r_osa_list[[1]] <- plot_osa_comps(obs=replist$Fishery_observed_and_expected_age_comp[,1:10],
+                     exp=replist$Fishery_observed_and_expected_age_comp[,11:20],
+                     ages=1:10, years=dat$fshyrs, Neff=dat$multN_fsh,
+                     model='ADMB final 2022')
+g_osa_list[[1]] <- plot_osa_comps(obs=replist$Fishery_observed_and_expected_age_comp[,1:10],
+                     exp=replist$Fishery_observed_and_expected_age_comp[,11:20],
+                     ages=1:10, years=dat$fshyrs, Neff=dat$multN_fsh,
+                     model='ADMB final 2022', plot.type='ggplot')
+
+# - TMB
 for(i in 1:length(quantities_list)){
-  r_osa_list[[i]] <- try(plot_osa_comps(obs=quantities_list[[i]]$res_fish[,1:10],
+  r_osa_list[[i+1]] <- try(plot_osa_comps(obs=quantities_list[[i]]$res_fish[,1:10],
                                     exp=quantities_list[[i]]$res_fish[,11:20],
                               ages=1:10,
                               years=dat$fshyrs,
                               Neff=dat$multN_fsh,
                               model=model_names[i],
                               plot.type='default'), silent = TRUE)
-  g_osa_list[[i]] <- try(plot_osa_comps(obs=quantities_list[[i]]$res_fish[,1:10],
+  g_osa_list[[i+1]] <- try(plot_osa_comps(obs=quantities_list[[i]]$res_fish[,1:10],
                                    exp=quantities_list[[i]]$res_fish[,11:20],
                               ages=1:10,
                               years=dat$fshyrs,
@@ -544,7 +579,7 @@ sapply(r_osa_list, class) # 1 doesnt work
 sapply(g_osa_list, class) # 1 and 5 doesnt work
 
 # Only show models that make sense (2D ar1 and 3d ar)
-cowplot::plot_grid(g_osa_list[[4]], g_osa_list[[7]], g_osa_list[[8]], g_osa_list[[9]])
+cowplot::plot_grid(g_osa_list[[1]], g_osa_list[[2]], g_osa_list[[5]], g_osa_list[[8]], g_osa_list[[9]], g_osa_list[[10]])
 
 
 
