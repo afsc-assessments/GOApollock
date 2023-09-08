6dbbfdf2d6f27a18439febb122e3a1e13542882e
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2023-09-08 12:59:28 -0700

    Quick code to explore output

--- TMB/R/plot_selectivity_models.R
+++ TMB/R/plot_selectivity_models.R
@@ -1,6 +1,33 @@
+library(tidyverse)
+theme_set(theme_bw())
 source("TMB/R/Functions/osa_comp_plots.R")
 source("TMB/R/Functions/construct_Q.R")
 source("TMB/R/Functions/plot_selectivity.R")
+load("TMB/Selectivity_runs.RData")
+years <- 1970:(2022+5)
+rho_trans <- function(x)2/(1+exp(-2*x))-1
+
+m7 <- data.frame(
+  par=names(sdrep_mod7$par.fixed),
+  est=sdrep_mod7$par.fixed,
+           se=sqrt(diag(sdrep_mod7$cov.fixed))) %>%
+  mutate(lwr=est-1.96*se, upr=est+1.96*se) %>%
+  filter(par %in% c("sel_rho_a", "sel_rho_y", "ln_sel_sd")) %>%
+  mutate(est=ifelse(par=='ln_sel_sd', exp(est), rho_trans(est)),
+         lwr=ifelse(par=='ln_sel_sd', exp(lwr), rho_trans(lwr)),
+         upr=ifelse(par=='ln_sel_sd', exp(upr), rho_trans(upr)))
+m7 %>% print(digits=3)
+
+m9 <- data.frame(
+  par=names(sdrep_mod9$par.fixed),
+  est=sdrep_mod9$par.fixed,
+           se=sqrt(diag(sdrep_mod9$cov.fixed))) %>%
+  mutate(lwr=est-1.96*se, upr=est+1.96*se) %>%
+  filter(par %in% c("sel_rho_a", "sel_rho_y", "ln_sel_sd")) %>%
+  mutate(est=ifelse(par=='ln_sel_sd', exp(est), est),
+         lwr=ifelse(par=='ln_sel_sd', exp(lwr), lwr),
+         upr=ifelse(par=='ln_sel_sd', exp(upr), upr))
+m9 %>% print(digits=3)
 
 ## Combine objects ----
 model_names <- c(
@@ -15,12 +42,13 @@ model_names <- c(
   'TMB-Mod9: 3D-AR1mar'
 )
 
+
 # - Sdrep objects
 mod_list <- list(sdrep_mod1, sdrep_mod2, sdrep_mod3, sdrep_mod4, sdrep_mod5, sdrep_mod6, sdrep_mod7, sdrep_mod8, sdrep_mod9)
 
 # - Opt objects
 opt_list <- list(opt_mod1, opt_mod2, opt_mod3, opt_mod4, opt_mod5, opt_mod6, opt_mod7, opt_mod8, opt_mod9)
-apply(opt_list, function(x) x$evaluations)
+lapply(opt_list, function(x) x$evaluations)
 
 obj_list <- list(obj_mod1, obj_mod2, obj_mod3, obj_mod4, obj_mod5, obj_mod6, obj_mod7, obj_mod8, obj_mod9)
 
@@ -71,6 +99,32 @@ ggplot(ssb, aes(year, est, color=model, fill=model, ymin=est-1.96*se, ymax=est+1
 # * Plot selectivity ----
 plot_selectivity(sdrep = mod_list,
                  model_names = model_names)
+
+get_fsh_selex <- function(x, model){
+  df <- data.frame(name=names(x$value), est=x$value, se=sqrt(diag(x$cov)))
+  df <- filter(df, name=='slctfsh') %>%
+    mutate(age=rep(1:10, each=length(years)),
+           year=rep(years, times=10), model=model)
+  df
+}
+out <- lapply(1:length(mod_list), function(x)
+  get_fsh_selex(mod_list[[x]], model_names[x])) %>%
+  bind_rows %>% filter(grepl(x=model, '4|7|9')) %>%
+  mutate(lwr=pmax(0, est-1*se), upr=pmin(1.5, est+1*se))
+ ## mutate(lwr=pmax(0, est-1.96*se), upr=pmin(1, est+1.96*se))
+
+ggplot(out, aes(year, est, color=model)) + facet_wrap('age', scales='free') +
+  geom_line()
+
+ggplot(out, aes(year, est, ymin=lwr, ymax=upr, fill=model, color=model)) + facet_wrap('age', scales='free') +
+  geom_line() + geom_ribbon(alpha=.3)
+ggplot(filter(out, age %in% 2:5), aes(year, est, ymin=lwr, ymax=upr, fill=model, color=model)) + facet_wrap('age', scales='free') +
+  geom_line() +## geom_ribbon(alpha=.3) +
+  xlim(2020,2027)
+
+ggplot(filter(out, year>2020), aes(age, est, ymin=lwr, ymax=upr, fill=model, color=model)) + facet_wrap('year', scales='free') +
+  geom_line() + geom_ribbon(alpha=.3)
+
 
 # * Plot OSA residuals ----
 quantities_list <- list(quantities_mod1, quantities_mod2, quantities_mod3, quantities_mod4, quantities_mod5, quantities_mod6, quantities_mod7, quantities_mod8, quantities_mod9)
