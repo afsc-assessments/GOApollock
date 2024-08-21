c2f687cbddc33ca081d153abc456e787b9fc3020
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2024-08-21 16:02:06 -0700

    Move read_dat function back into R package

--- R/tmb_fns.R
+++ R/tmb_fns.R
@@ -1,3 +1,183 @@
+#' Read text file input for model. Excel sheet is saved as .txt and read into R with this function.
+#'
+#' @param filename The filename to be read in
+#' @param path Path to the file if not in working directory
+#' @return A named list of all elements with corresponding names
+#'   to the ADMB model
+read_pk_dat <- function(filename, path=NULL, writedat=FALSE){
+  if(!is.null(path)){
+    oldwd <- getwd()
+    on.exit(setwd(oldwd))
+    setwd(path)
+  }
+  if(!file.exists(filename)) stop("File ", filename, " does not exist")
+  char.lines <- suppressMessages(readLines(filename, warn=FALSE))
+  com.ind <- which(substring(char.lines, 1, 1) %in% c(" ", "#"))
+  blank.ind <- which(substring(char.lines, 1, 1) == "\t")
+  com.ind <- sort(c(com.ind,blank.ind))
+  dat.start <- com.ind[c(which(diff(com.ind) > 1), length(com.ind))]
+  comments <- char.lines[dat.start]
+  ## dat <- char.lines[-com.ind]
+  ## ## drop anything after # on that line
+  ## dat <- lapply(strsplit(dat, split='#'), function(x) x[1])
+  d <- list()
+  ind <- 0
+  si <- function(ind, n, nrow=NULL, ncol=NULL){
+    s <- tryCatch(scan(filename, quiet = T, what = integer(), comment.char="#",
+                       skip=dat.start[ind <<- ind+1], n=n),
+                  error=function(e) 'error')
+    if(is.character(s)) {print(d); stop("failed to read integer")}
+    if(is.null(nrow)) return(s)
+    return(matrix(s, nrow=nrow, ncol=ncol, byrow=TRUE))
+  }
+  sn <- function(ind, n, nrow=NULL, ncol=NULL){
+    s <- scan(filename, quiet = T, what = numeric(), comment.char="#",
+              skip=dat.start[ind <<- ind+1], n=n)
+    if(is.null(nrow)) return(s)
+    return(matrix(s, nrow=nrow, ncol=ncol, byrow=TRUE))
+  }
+  if(writedat){
+    d$styr <- si(ind, n=1)
+    d$endyr <- si(ind,n=1)
+  } else {
+    tmp <- si(ind , n = 2)
+    d$styr <- tmp[1]; d$endyr <- tmp[2]
+  }
+  nyrs <- d$endyr-d$styr+1
+  nyrs2 <- nyrs-1
+  if(writedat){
+    d$rcrage <- si(ind,n=1)
+    d$trmage <- si(ind,n=1)
+  } else {
+    tmp <- si(ind=ind, n = 2)
+    d$rcrage <- tmp[1]; d$trmage <- tmp[2]
+  }
+  maxage <- d$trmage
+  if(writedat){
+    d$nbins1 <- si(ind,n=1)
+    d$nbins2 <- si(ind,n=1)
+    d$nbins3 <- si(ind,n=1)
+  } else {
+    tmp <- si(ind=ind, n = 3)
+    d$nbins1 <- tmp[1]; d$nbins2 <- tmp[2]; d$nbins3 <- tmp[3]
+  }
+  d$cattot <- sn( ind=ind, n = nyrs)
+  d$cattot_log_sd <- sn( ind=ind, n = nyrs)
+  d$nyrs_fsh <- si(ind=ind, n =1)
+  d$fshyrs <- sn( ind=ind, n = d$nyrs_fsh)
+  d$multN_fsh <- sn( ind=ind, n = d$nyrs_fsh)
+  d$ac_yng_fsh <- si(ind=ind, n = d$nyrs_fsh)
+  d$ac_old_fsh <- si(ind=ind, n = d$nyrs_fsh)
+  d$nyrslen_fsh <- si(ind=ind, n =1)
+  d$fshlenyrs <- sn( ind=ind, n = d$nyrslen_fsh)
+  d$multNlen_fsh <- sn( ind=ind, n = d$nyrslen_fsh)
+  d$rwlk_sd <- sn( ind=ind, n = nyrs2)
+  d$catp <- sn( ind=ind, n = d$nyrs_fsh*maxage, nrow=d$nyrs_fsh, ncol=maxage)
+  d$lenp <- sn( ind=ind, n = d$nyrslen_fsh*d$nbins1, nrow=d$nyrslen_fsh, ncol=d$nbins1)
+  d$wt_fsh <- sn( ind=ind, n = nyrs*maxage, nrow=nyrs, ncol=maxage)
+  ## The used survey 1
+  d$nyrs_srv1 <- si(ind=ind, n =1)
+  d$srvyrs1 <- si(ind=ind, n =d$nyrs_srv1)
+  d$indxsurv1 <- sn( ind=ind, n =d$nyrs_srv1)
+  d$indxsurv_log_sd1 <- sn( ind=ind, n =d$nyrs_srv1)
+  d$q1_rwlk_sd <- sn( ind=ind, n = nyrs2)
+  d$yrfrct_srv1 <- sn( ind=ind, n = nyrs)
+  d$nyrsac_srv1 <- si(ind=ind, n =1)
+  d$srv_acyrs1 <- sn( ind=ind, n = d$nyrsac_srv1)
+  d$multN_srv1 <- sn( ind=ind, n =d$nyrsac_srv1)
+  d$ac_yng_srv1 <- tmp <- si(ind=ind, n = d$nyrsac_srv1)
+  d$ac_old_srv1 <- tmp <- si(ind=ind, n = d$nyrsac_srv1)
+  d$nyrslen_srv1 <- si(ind=ind, n =1)
+  d$srv_lenyrs1 <- sn( ind=ind, n = d$nyrslen_srv1)
+  d$multNlen_srv1 <- sn( ind=ind, n =d$nyrslen_srv1)
+  d$srvp1 <- sn(ind=ind, n =d$nyrsac_srv1*maxage,
+                nrow=d$nyrsac_srv1, ncol=maxage)
+  d$srvlenp1 <- sn(ind=ind, n =d$nyrslen_srv1*d$nbins3,
+                   nrow=d$nyrslen_srv1, ncol=d$nbins3)
+  d$wt_srv1 <- sn( ind=ind, n = nyrs*maxage, nrow=nyrs, ncol=maxage)
+  ## Survey 2
+  d$nyrs_srv2 <- si(ind=ind, n =1)
+  d$srvyrs2 <- si(ind=ind, n =d$nyrs_srv2)
+  d$indxsurv2 <- sn( ind=ind, n =d$nyrs_srv2)
+  d$indxsurv_log_sd2 <- sn( ind=ind, n =d$nyrs_srv2)
+  d$q2_rwlk_sd <- sn( ind=ind, n = nyrs2)
+  d$yrfrct_srv2 <- sn( ind=ind, n = nyrs)
+  d$nyrsac_srv2 <- si(ind=ind, n =1)
+  d$srv_acyrs2 <- sn( ind=ind, n = d$nyrsac_srv2)
+  d$multN_srv2 <- sn( ind=ind, n =d$nyrsac_srv2)
+  d$ac_yng_srv2 <- si(ind=ind, n = d$nyrsac_srv2)
+  d$ac_old_srv2 <- si(ind=ind, n = d$nyrsac_srv2)
+  d$nyrslen_srv2 <- si(ind=ind, n =1)
+  d$srv_lenyrs2 <- sn( ind=ind, n = d$nyrslen_srv2)
+  d$multNlen_srv2 <- sn( ind=ind, n =d$nyrslen_srv2)
+  d$srvp2 <- sn(ind=ind, n =d$nyrsac_srv2*maxage,
+                nrow=d$nyrsac_srv2, ncol=maxage)
+  d$srvlenp2 <- sn(ind=ind, n =d$nyrslen_srv2*d$nbins3,
+                   nrow=d$nyrslen_srv2, ncol=d$nbins3)
+  d$wt_srv2 <- sn( ind=ind, n = nyrs*maxage, nrow=nyrs, ncol=maxage)
+  ## Survey 3
+  d$nyrs_srv3 <- si(ind=ind, n =1)
+  d$srvyrs3 <- si(ind=ind, n =d$nyrs_srv3)
+  d$indxsurv3 <- sn( ind=ind, n =d$nyrs_srv3)
+  d$indxsurv_log_sd3 <- sn( ind=ind, n =d$nyrs_srv3)
+  d$q3_rwlk_sd <- sn( ind=ind, n = nyrs2)
+  d$yrfrct_srv3 <- sn( ind=ind, n = nyrs)
+  d$nyrsac_srv3 <- si(ind=ind, n =1)
+  d$srv_acyrs3 <- sn( ind=ind, n = d$nyrsac_srv3)
+  d$multN_srv3 <- sn( ind=ind, n =d$nyrsac_srv3)
+  ## tmp <- si(##                    ind=ind, n = 2*d$nyrsac_srv3)
+  ## d$ac_yng_srv3 <- tmp[1:d$nyrs_srv3]; d$ac_old_srv3 <- tmp[-(1:d$nyrs_srv3)]
+  d$nyrslen_srv3 <- si(ind=ind, n =1)
+  d$srv_lenyrs3 <- sn( ind=ind, n = d$nyrslen_srv3)
+  d$multNlen_srv3 <- sn( ind=ind, n =d$nyrslen_srv3)
+  d$srvp3 <- sn(ind=ind, n =d$nyrsac_srv3*maxage, nrow=d$nyrsac_srv3, ncol=maxage)
+  d$srvlenp3 <- sn(ind=ind, n =d$nyrslen_srv3*d$nbins3, nrow=d$nyrslen_srv3, ncol=d$nbins3)
+  d$wt_srv3 <- sn( ind=ind, n = nyrs*maxage, nrow=nyrs, ncol=maxage)
+  ## survey 4
+  d$nyrs_srv4 <- si(ind=ind, n =1)
+  d$srvyrs4 <- si(ind=ind, n =d$nyrs_srv4)
+  d$indxsurv4 <- sn( ind=ind, n =d$nyrs_srv4)
+  d$indxsurv_log_sd4 <- sn( ind=ind, n =d$nyrs_srv4)
+  ## survey 5
+  d$nyrs_srv5 <- si(ind=ind, n =1)
+  d$srvyrs5 <- si(ind=ind, n =d$nyrs_srv5)
+  d$indxsurv5 <- sn( ind=ind, n =d$nyrs_srv5)
+  d$indxsurv_log_sd5 <- sn( ind=ind, n =d$nyrs_srv5)
+  ## Survey 6
+  d$nyrs_srv6 <- si(ind=ind, n =1)
+  d$srvyrs6 <- si(ind=ind, n =d$nyrs_srv6)
+  d$indxsurv6 <- sn( ind=ind, n =d$nyrs_srv6)
+  d$indxsurv_log_sd6 <- sn( ind=ind, n =d$nyrs_srv6)
+  ## d$q6_rwlk_sd <- sn( ##                    ind=ind, n = nyrs2)
+  d$yrfrct_srv6 <- sn( ind=ind, n = nyrs)
+  d$nyrsac_srv6 <- si(ind=ind, n =1)
+  d$srv_acyrs6 <- sn( ind=ind, n = d$nyrsac_srv6)
+  d$multN_srv6 <- sn( ind=ind, n =d$nyrsac_srv6)
+  d$ac_yng_srv6 <-si(ind=ind, n = d$nyrsac_srv6)
+  d$ac_old_srv6 <- si(ind=ind, n = d$nyrsac_srv6)
+  d$nyrslen_srv6 <- si(ind=ind, n =1)
+  d$srv_lenyrs6 <- sn( ind=ind, n = d$nyrslen_srv6)
+  d$multNlen_srv6 <- sn( ind=ind, n =d$nyrslen_srv6)
+  d$srvp6 <- sn(ind=ind, n =d$nyrsac_srv6*maxage,
+                nrow=d$nyrsac_srv6, ncol=maxage)
+  d$srvlenp6 <- sn(ind=ind, n =d$nyrslen_srv6*d$nbins2,
+                   nrow=d$nyrslen_srv6, ncol=d$nbins2)
+  d$wt_srv6 <- sn( ind=ind, n = nyrs*maxage, nrow=nyrs, ncol=maxage)
+  d$age_trans <- sn( ind=ind, n = maxage*maxage, nrow=maxage, ncol=maxage)
+  d$len_trans1 <- sn( ind=ind, n = d$nbins1*maxage, nrow=maxage, ncol=d$nbins1)
+  d$len_trans2 <- sn( ind=ind, n = d$nbins2*maxage, nrow=maxage, ncol=d$nbins2)
+  d$len_trans3 <- sn( ind=ind, n = d$nbins3*maxage, nrow=maxage, ncol=d$nbins3)
+  ## the projection module inputs
+  d$mat <- sn( ind=ind, n =maxage)
+  d$Ftarget <- sn( ind=ind, n =5)
+  d$B40 <- sn( ind=ind, n =1)
+  d$log_mean_recr_proj <- sn( ind=ind, n =1)
+  d$sigmasq_recr <- sn( ind=ind, n =1)
+  d$check <- sn(ind=ind, n=1)
+  if(d$check != -999) stop("Failed to read in dat file, final value=",d$check)
+  return(d)
+}
+
 
 #' Extract sdreport data.frames from fitted models
 #' @param fits A single model or list of models as returned by
@@ -163,13 +343,13 @@ fit_pk <- function(input, getsd=TRUE, newtonsteps=1,
 #' @param random A character vector declaring random effect
 #'   vectors to be integrated. Defaults to NULL (fully penalized
 #'   ML)
-#' @param modfile Model name assumed to be 'goa_pk_tmb' unless
+#' @param modfile Model name assumed to be 'goa_pk' unless
 #'   specified
 #' @return A list with the version, dat, pars, map and random
 #'   which are used to build a TMB 'obj' in \code{\link{fit_pk}}.
 #' @export
 prepare_pk_input <- function(path, datfile, version='none',
-                             random=NULL, modfile='goa_pk_tmb'){
+                             random=NULL, modfile='goa_pk'){
   if(!dir.exists(path)) stop("directory does not exist: ",path)
   dat <- read_pk_dat(filename=datfile, path=path)
   ## Prepare the parameter list based on the data
